f1e23e039568c3493c35135449d84d75fcd5707d
abhi5ek <abhishekpandey7676@gmail.com>
2024-10-14 13:26:10 +0530

    category

---
+++ src/views/categoryManagement/doorManagement/FiberGlassEntry.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FiberGlassEntry = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FiberGlassEntry
---
+++ src/views/categoryManagement/doorManagement/FiberGlassFrench.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FiberGlassFrench = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FiberGlassFrench
